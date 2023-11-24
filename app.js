0ef17368688f3e9bb8b932000cfe959a5f1ec129
RuanVanAsten <126241288+RuanVanAsten@users.noreply.github.com>
2023-11-24 10:28:38 +0100

    login

--- app.js
+++ app.js
@@ -1,34 +1,19 @@
 // Definieer het aangepaste inlogcomponent
 class LoginComponent extends HTMLElement {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        // Creëer een Shadow DOM
-        this.attachShadow({ mode: 'open' });
+    // Creëer een Shadow DOM
+    this.attachShadow({ mode: 'open' });
 
-        // Voeg stijlen toe aan het Shadow DOM
-        this.shadowRoot.innerHTML = `
+    // Voeg stijlen toe aan het Shadow DOM
+    this.shadowRoot.innerHTML = `
       <style>
         :host {
           display: block;
           text-align: center;
-        }
-        form {
-          width: 300px;
           padding: 20px;
           background-color: #fff;
-          border-radius: 8px;
-          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-        label {
-          display: block;
-          margin-bottom: 8px;
-        }
-        input {
-          width: 100%;
-          padding: 8px;
-          margin-bottom: 16px;
-          box-sizing: border-box;
         }
         button {
           background-color: #007bff;
@@ -39,25 +24,50 @@ class LoginComponent extends HTMLElement {
           cursor: pointer;
         }
       </style>
-      <form>
-        <label for="username">Email:</label>
-        <input type="text" id="username" name="username" required>
-        <label for="password">Wachtwoord:</label>
-        <input type="password" id="password" name="password" required>
-        <button type="button" id="loginButton">Inloggen</button>
-      </form>
+      <div>
+        <button id="loginButton">Inloggen</button>
+      </div>
     `;
-    }
+  }
 
-    // Wordt aangeroepen wanneer het element aan het DOM wordt toegevoegd
-    connectedCallback() {
-        // Voeg een click-eventlistener toe aan de inlogknop
-        this.shadowRoot.getElementById('loginButton').addEventListener('click', () => {
-            // Voeg hier inlogfunctionaliteit toe
-            alert('Inloggen gelukt!');
-        });
-    }
+  // Wordt aangeroepen wanneer het element aan het DOM wordt toegevoegd
+  connectedCallback() {
+    // Voeg een click-eventlistener toe aan de inlogknop
+    this.shadowRoot.getElementById('loginButton').addEventListener('click', () => {
+      // Voeg hier inlogfunctionaliteit toe
+      alert('Inlogpoging!');
+    });
+  }
 }
 
 // Registreer het aangepaste inlogcomponent
 customElements.define('login-component', LoginComponent);
+
+// Definieer het aangepaste footercomponent
+class FooterComponent extends HTMLElement {
+  constructor() {
+    super();
+
+    // Creëer een Shadow DOM
+    this.attachShadow({ mode: 'open' });
+
+    // Voeg stijlen toe aan het Shadow DOM
+    this.shadowRoot.innerHTML = `
+      <style>
+        :host {
+          display: block;
+          text-align: center;
+          padding: 20px;
+          background-color: #333;
+          color: #fff;
+        }
+      </style>
+      <div>
+        Footer-tekst hier
+      </div>
+    `;
+  }
+}
+
+// Registreer het aangepaste footercomponent
+customElements.define('footer-component', FooterComponent);
