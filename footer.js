cb7baae8610f9aa856468f1dbb4cd230e32afb72
RuanVanAsten <126241288+RuanVanAsten@users.noreply.github.com>
2023-12-01 10:00:00 +0100

    loginpage
    header, footer toegevoegd + login gebruikt van steffen

---
+++ footer.js
@@ -0,0 +1,30 @@
+class FooterComponent extends HTMLElement {
+    constructor() {
+        super();
+
+        // Creëer een Shadow DOM
+        this.attachShadow({ mode: 'open' });
+
+        // Voeg stijlen toe aan het Shadow DOM
+        this.shadowRoot.innerHTML = `
+      <style>
+        :host {
+          display: block;
+          width: 100%;
+          background-color: #333;
+          color: white;
+          text-align: center;
+          padding: 10px;
+          position: fixed;
+          bottom: 0;
+        }
+      </style>
+      <div>
+        Project Brievenbus
+      </div>
+    `;
+    }
+}
+
+// Registreer het aangepaste footercomponent
+customElements.define('footer-component', FooterComponent);
---
+++ header.js
@@ -0,0 +1,38 @@
+class HeaderComponent extends HTMLElement {
+    constructor() {
+        super();
+
+        // Creëer een Shadow DOM
+        this.attachShadow({ mode: 'open' });
+
+        // Voeg stijlen toe aan het Shadow DOM
+        this.shadowRoot.innerHTML = `
+      <style>
+        :host {
+          display: block;
+          background-color: #333;
+          color: white;
+          width: 100%;
+          text-align: center;
+          position:fixed;
+          top: 0;
+          padding: 10px;
+        }
+      </style>
+      <div id="header">
+        <h1>Your Website</h1>
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+        </nav>
+      </div>
+    `;
+    }
+}
+
+// Registreer het aangepaste headercomponent
+customElements.define('header-component', HeaderComponent);
+
+
+
